fffce1d94df32bbdf4329e3eb461aca23927c26d
pateras@gmail.com <pateras@gmail.com>
2018-04-21 16:54:16 -0400

    removed Library

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
     private float angle = 0;
+    private float velocity = 30.0f;
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Hey there.");
+        Debug.Log("Hey there. I have been updated!");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.localEulerAngles = new Vector3(0, angle, 0);
-        angle += 10 * Time.deltaTime;
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase == UnityEngine.TouchPhase.Began)
+        {
+            velocity *= -1;
+        }
+
+        transform.localEulerAngles = new Vector3(angle / 2.0f, angle, 0);
+        angle += velocity * Time.deltaTime;
 	}
 }
