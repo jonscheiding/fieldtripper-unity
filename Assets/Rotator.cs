5bfbf755365d8964d7a706c60d665c3a3ec287a7
Jon Scheiding <jonscheiding@gmail.com>
2018-04-21 16:10:01 -0400

    Initial commit

---
+++ Assets/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+    private float angle = 0;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hey there.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localEulerAngles = new Vector3(0, angle, 0);
+        angle += 10 * Time.deltaTime;
+	}
+}
