b0b595db39309152780d634960dab17e18562352
pateras <pateras>
2018-04-22 10:32:53 -0400

    Added shade and set Deployment Target.

---
+++ Assets/_FieldTripper/scripts/ScreenShade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets._FieldTripper.scripts
+{
+	public class ScreenShade : MonoBehaviour
+	{
+		[SerializeField]
+		private Image ShadeImage;
+
+		private void Start()
+		{
+			ShadeImage.rectTransform.anchoredPosition = new Vector2(0.0f, -Screen.height);
+			ShadeImage.transform.Translate(new Vector3(0.0f, 100.0f, 0.0f));
+		}
+	}
+}
